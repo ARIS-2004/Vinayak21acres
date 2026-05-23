195a1fea8bc1a721e9bc4aab959e469a378326ba
Aris <shaikharisakhtar1234@gmail.com>
2026-05-23 15:33:17 +0530

    Inline GTM script directly in <head> per Google's snippet

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 const FAVICON = "/favicon_io (17)";
@@ -28,13 +27,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         {/* Google Tag Manager */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}
-        </Script>
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
         {/* End Google Tag Manager */}
       </head>
       <body>
